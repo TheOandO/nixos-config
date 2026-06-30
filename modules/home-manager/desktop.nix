67c736b5ec5a636c7215b6d8f546a4195008591c
matty <realkripper@gmail.com>
2026-06-30 13:39:49 +0200

    update: 2026-06-30

--- modules/home-manager/desktop.nix
+++ modules/home-manager/desktop.nix
@@ -9,17 +9,17 @@
 	};
 
 	programs.fish.functions.rebuild = ''
-	  	cd /etc/nixos
-	  	sudo git fetch
-	  	sudo git pull
-	  	sudo git add .
-	  	read -P "Commit message (leave empty for default): " msg
-	  	if test -z "$msg"
-	    	set msg "update: "(date +%Y-%m-%d)
-	  	end
-	  	sudo git commit -m "$msg"; or true
-	  	sudo git push
-	  	sudo nix flake update
+		cd /etc/nixos
+		sudo git add .
+		read -P "Commit message (leave empty for default): " msg
+		if test -z "$msg"
+		    set msg "update: "(date +%Y-%m-%d)
+		end
+		sudo git commit -m "$msg"; or true
+		sudo git fetch origin
+		sudo git rebase -X ours origin/main; or true
+		sudo git push --force
+		sudo nix flake update
 	  	sudo nixos-rebuild switch --upgrade-all --flake .#desktop
 	'';
 }
