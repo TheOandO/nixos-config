ea02dae7f471c75162faf37a8fc0c29915f3c405
matty <realkripper@gmail.com>
2026-06-30 13:54:42 +0200

    implement steam

--- modules/hosts/desktop/programs.nix
+++ modules/hosts/desktop/programs.nix
@@ -14,6 +14,7 @@
 		libsForQt5.qt5ct
 		kdePackages.qt6ct
 		heroic
+		protonplus
 	];
 
 	programs.hyprland.enable = true;
@@ -42,4 +43,14 @@
 	    	};
 	    }
 	];
+
+	programs.steam = {
+	  enable = true; # Master switch, already covered in installation
+	  remotePlay.openFirewall = true;  # Open ports in the firewall for Steam Remote Play
+	  dedicatedServer.openFirewall = true; # Open ports for Source Dedicated Server hosting
+	  # Other general flags if available can be set here.
+	};
+
+	programs.gamemode.enable = true;
+	
 }
