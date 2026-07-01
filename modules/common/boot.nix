6cb78f03cd133695fed92adc01230e5c422a1193
matty <realkripper@gmail.com>
2026-07-01 19:20:46 +0200

    improving gaming

--- modules/common/boot.nix
+++ modules/common/boot.nix
@@ -2,7 +2,9 @@
 
 {
 	# Bootloader.
-  	boot.loader.systemd-boot.enable = true;
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "nodev";
+    boot.loader.grub.useOSProber = true;
   	boot.loader.efi.canTouchEfiVariables = true;
 
   	# Use latest kernel.
--- modules/hosts/desktop/boot.nix
+++ modules/hosts/desktop/boot.nix
@@ -7,7 +7,7 @@
 
   	# Use latest kernel.
   	# boot.kernelPackages = pkgs.linuxPackages_latest;
-  	boot.loader.grub.enable = true;
-  	boot.loader.grub.device = "nodev";
-  	boot.loader.grub.useOSProber = true;
+  	#boot.loader.grub.enable = true;
+  	#boot.loader.grub.device = "nodev";
+  	#boot.loader.grub.useOSProber = true;
 }
