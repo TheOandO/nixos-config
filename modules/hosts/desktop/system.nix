13626ea6d90f2759bf222099fbb176b85f158443
matty <realkripper@gmail.com>
2026-07-01 22:52:28 +0200

    WIP game almost playable

--- modules/hosts/desktop/system.nix
+++ modules/hosts/desktop/system.nix
@@ -3,17 +3,44 @@
 {
 	fileSystems."/mnt/ext" =
 	{ device = "/dev/disk/by-uuid/5EEFD218069F2932";
-		fsType = "ntfs3";
+		fsType = "ntfs-3g";
+		options = [ 
+			  "rw" 
+			  "uid=1000" 
+			  "gid=100" 
+			  "dmask=022" 
+			  "fmask=133" 
+			  "nofail"
+			  "windows_names"
+		];
 	};
 
 	fileSystems."/mnt/hihi" =
 	{ device = "/dev/disk/by-uuid/4A1C39811C396959";
-	    fsType = "ntfs3";
+		fsType = "ntfs-3g";
+		options = [ 
+			  "rw" 
+			  "uid=1000" 
+			  "gid=100" 
+			  "dmask=022" 
+			  "fmask=133" 
+			  "nofail"
+			  "windows_names"
+		];
 	};
 
 	fileSystems."/mnt/adu" =
 	{ device = "/dev/disk/by-uuid/3AD8F535D8F4EFCD";
-	    fsType = "ntfs3";
+		fsType = "ntfs-3g";
+		options = [ 
+			  "rw" 
+			  "uid=1000" 
+			  "gid=100" 
+			  "dmask=022" 
+			  "fmask=133" 
+			  "nofail"
+			  "windows_names"
+		];
 	};
 
 }
