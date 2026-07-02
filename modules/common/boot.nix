b6c6c5c6d452c72a47418d9c1c9f1cfd48e2699c
matty <realkripper@gmail.com>
2026-07-02 23:38:33 +0200

    migrate to limine

--- modules/common/boot.nix
+++ modules/common/boot.nix
@@ -2,9 +2,7 @@
 
 {
 	# Bootloader.
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = "nodev";
-    boot.loader.grub.useOSProber = true;
+    boot.loader.limine.enable = true;
   	boot.loader.efi.canTouchEfiVariables = true;
 
   	# Use latest kernel.
