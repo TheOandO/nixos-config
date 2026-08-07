e7b80e94a357159903ecefe97f01e31b266a6f22
matty <realkripper@gmail.com>
2026-08-07 21:46:36 +0200

    add shortcuts for fish

--- modules/home-manager/desktop.nix
+++ modules/home-manager/desktop.nix
@@ -65,18 +65,34 @@
 	  };
 	};
 	
-	programs.fish.functions.rebuild = ''
-		cd /etc/nixos
-		sudo nix flake update
-		sudo git add .
-		read -P "Commit message (leave empty for default): " msg
-		if test -z "$msg"
-		    set msg "update: "(date +%Y-%m-%d)
-		end
-		sudo git commit -m "$msg"; or true
-		sudo git fetch origin
-		sudo git rebase -X ours origin/main; or true
-		sudo git push --force
-	  	sudo nixos-rebuild switch --upgrade-all --flake .#desktop
-	'';
+	programs.fish.functions = {
+		rebuild = ''
+				cd /etc/nixos
+				sudo nix flake update
+				sudo git add .
+				read -P "Commit message (leave empty for default): " msg
+				if test -z "$msg"
+				    set msg "update: "(date +%Y-%m-%d)
+				end
+				sudo git commit -m "$msg"; or true
+				sudo git fetch origin
+				sudo git rebase -X ours origin/main; or true
+				sudo git push --force
+			  	sudo nixos-rebuild switch --upgrade-all --flake .#desktop
+			'';	
+		pull-nix = ''
+			cd /etc/nixos
+			sudo git stash
+			sudo git pull
+			sudo git clear
+			sudo nixos-rebuild switch --upgrade-all --flake .#desktop
+		'';
+		pull-dot = ''
+			cd ~/.config
+			sudo git stash
+			sudo git pull
+			sudo git clear
+		'';
+	};
+
 }
