b04dcfa2e4ff89a128e1b8354fab2a99bbba622c
matty <realkripper@gmail.com>
2026-08-10 15:03:16 +0200

    added credentials for NAS persistent volumn

--- modules/hosts/desktop/system.nix
+++ modules/hosts/desktop/system.nix
@@ -15,4 +15,21 @@
 	{ device = "/dev/disk/by-uuid/40a40c3d-72e7-4ad2-a2fa-6e93ff0cf9fb";
 		fsType = "ext4";
 	};
+
+	fileSystems."/mnt/nas" = {
+		  device = "//mattyomv.local/NAS/";
+		  fsType = "cifs";
+		  options = let
+		    	# automount options: only mounts on first access, unmounts after idle
+		    	automount_opts = "x-systemd.automount,noauto";
+		  in [
+		    	"credentials=/etc/nixos/credentials/omv"
+		    	"_netdev"
+		    	"uid=1000"
+		    	"gid=100"
+		    	"file_mode=0664"
+		    	"dir_mode=0775"
+		    automount_opts
+		  ];
+	};
 }
