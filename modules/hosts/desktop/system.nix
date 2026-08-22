cc34c2feceb705c545cccfe1448f305abf3eafab
matty <realkripper@gmail.com>
2026-08-22 20:04:49 +0200

    hihi

--- modules/hosts/desktop/system.nix
+++ modules/hosts/desktop/system.nix
@@ -2,18 +2,21 @@
 
 {
 	fileSystems."/mnt/ext" =
-	{ device = "/dev/disk/by-uuid/f3d7288c-184d-4195-b001-9f9a9ecb576c";
-		fsType = "ext4";
+	{ device = "/dev/disk/by-uuid/C6AC6908AC68F479";
+		fsType = "ntfs3";
+		options = [ "nofail" ];
 	};
 
 	fileSystems."/mnt/hihi" =
 	{ device = "/dev/disk/by-uuid/d65ec31a-067e-4bf1-87d8-5f7c4e5297e3";
 		fsType = "ext4";
+		options = [ "nofail" ];
 	};
 
 	fileSystems."/mnt/adu" =
 	{ device = "/dev/disk/by-uuid/40a40c3d-72e7-4ad2-a2fa-6e93ff0cf9fb";
 		fsType = "ext4";
+		options = [ "nofail" ];
 	};
 
 	fileSystems."/mnt/nas" = {
