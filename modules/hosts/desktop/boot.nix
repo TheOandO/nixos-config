d2b5fadb0f49e13bc826942b3790c2a1484509d1
matty <realkripper@email.com>
2026-09-22 17:27:09 +0200

    [desktop] feat: workaround to prevent Windows from deleting NixOS bootloader

--- modules/hosts/desktop/boot.nix
+++ modules/hosts/desktop/boot.nix
@@ -6,4 +6,10 @@
 		secureBoot.enable = true;
 		resolution = "1920x1080";
 	};
+
+	#Workaround script for disappearing bootloader
+	system.activationScripts.limine-sync = ''
+		mkdir -p /boot/efi/Boot
+		cp -f /boot/efi/limine/BOOTX64.EFI /boot/efi/Boot/
+	'';
 }
